6695035a436724f44d3f8d9badc7a9faa03d1f65
GaussianBlurs <arnoldvbg@gmail.com>
2018-12-06 16:28:22 +0100

    Install packages

---
+++ codex/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "extends": ["airbnb", "plugin:react/recommended", "plugin:jsx-a11y/recommended"],
+  "plugins": [
+    "jsx-a11y",
+    "react"
+  ],
+  "parser": "babel-eslint",
+  "rules": {
+    "space-in-parens": "off",
+    "template-curly-spacing": [2, "never"],
+    "object-curly-spacing": [2, "always"],
+    "object-curly-newline": "off",
+    "no-use-before-define": [2, { "functions": false }],
+    "semi": [2, "never"],
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/jsx-one-expression-per-line": [0, { "allow": "single-child" }],
+    "react/destructuring-assignment": "off",
+    "operator-linebreak": [2, "after"],
+    "comma-dangle": [2, "never"],
+    "no-param-reassign": [1],
+    "no-underscore-dangle": "off",
+    "no-console": [2, { "allow": ["warn", "error"] }]
+  },
+  "env": {
+    "browser": true,
+    "jest": true
+  }
+};
