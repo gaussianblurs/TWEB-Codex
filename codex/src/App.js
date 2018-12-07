32b8cbc96d621202b73925c0253d2af1785cfca2
GaussianBlurs <arnoldvbg@gmail.com>
2018-12-07 09:46:21 +0100

    Minor modifications

--- codex/src/App.js
+++ codex/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import Main from './components/Main'
 import Header from './components/header/Header'
 import withAuthentication from './components/withAuthentication'
 
 const App = () => (
-  <div>
-    <Header />
-    <Main />
-  </div>
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Main />
+    </div>
+  </BrowserRouter>
 )
 
 export default withAuthentication(App)
