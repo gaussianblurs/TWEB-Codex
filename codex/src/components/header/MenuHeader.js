aca648e20e05f0255a3e3daf742733d6fca948d0
GaussianBlurs <arnoldvbg@gmail.com>
2018-12-07 16:20:59 +0100

    Header refactoring

--- codex/src/components/header/MenuHeader.js
+++ codex/src/components/header/MenuHeader.js
@@ -1,40 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Button } from 'semantic-ui-react'
+import { Menu, Container, Segment, Button } from 'semantic-ui-react'
 import * as routes from '../../constants/routes'
 import { auth } from '../../firebase'
 import AuthUserContext from '../AuthUserContext'
 
 const AuthNav = () => (
   <React.Fragment>
-    <Button.Group floated="right">
-      <Button onClick={auth.doSignOut}>Sign out</Button>
-    </Button.Group>
+    <Button className="item" floated="right" onClick={auth.doSignOut}>Sign out</Button>
   </React.Fragment>
 )
 
 const NonAuthNav = () => (
   <React.Fragment>
-    <Link className="item" to={routes.SIGN_IN}>
-      Sign in
-    </Link>
-    <Link className="item" to={routes.SIGN_UP}>
-      Sign up
-    </Link>
+    <Menu.Item position="right">
+      <Link to={routes.SIGN_IN}>
+        Sign in
+      </Link>
+      <Link to={routes.SIGN_UP} style={{ marginLeft: '0.5em' }}>
+        Sign up
+      </Link>
+    </Menu.Item>
   </React.Fragment>
 )
 
 const MenuHeader = () => (
-  <div className="ui inverted menu">
-    <Link className="active item" to={routes.HOME}>
-      CODEX
-    </Link>
-    <AuthUserContext.Consumer>
-      {
-        ({ authUser }) => (authUser ? <AuthNav /> : <NonAuthNav />)
-      }
-    </AuthUserContext.Consumer>
-  </div>
+  <Segment inverted>
+    <Container>
+      <Menu inverted pointing secondary>
+        <Menu.Item as={Link} to={routes.HOME}>
+          CODEX
+        </Menu.Item>
+        <AuthUserContext.Consumer>
+          {
+            ({ authUser }) => (authUser ? <AuthNav /> : <NonAuthNav />)
+          }
+        </AuthUserContext.Consumer>
+      </Menu>
+    </Container>
+  </Segment>
 )
 
 export default MenuHeader
