57a6125bb4e607cf9160804ab0964d03945f4e06
psr <pierre.samuel.rochat@gmail.com>
2018-12-07 13:38:50 +0100

    import spinner and font awsome

--- codex/src/App.js
+++ codex/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { library } from '@fortawesome/fontawesome-svg-core'
 import Main from './components/Main'
 import Header from './components/header/Header'
 import withAuthentication from './components/withAuthentication'
