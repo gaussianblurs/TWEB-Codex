e424c200bc6481f30f991e89d3a960041c02638e
GaussianBlurs <arnoldvbg@gmail.com>
2018-12-07 15:22:08 +0100

    AuthNav / NonAuthNav

--- codex/src/components/header/MenuHeader.js
+++ codex/src/components/header/MenuHeader.js
@@ -1,18 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import * as routes from '../../constants/routes'
+import AuthUserContext from '../AuthUserContext'
 
-const MenuHeader = () => (
-  <div className="ui inverted menu">
-    <Link className="active item" to={routes.HOME}>
-      CODEX
-    </Link>
+const AuthNav = () => (
+  <React.Fragment />
+)
+
+const NonAuthNav = () => (
+  <React.Fragment>
     <Link className="item" to={routes.SIGN_IN}>
       Sign in
     </Link>
     <Link className="item" to={routes.SIGN_UP}>
       Sign up
     </Link>
+  </React.Fragment>
+)
+
+const MenuHeader = () => (
+  <div className="ui inverted menu">
+    <Link className="active item" to={routes.HOME}>
+      CODEX
+    </Link>
+    <AuthUserContext.Consumer>
+      {
+        ({ authUser }) => (authUser ? <AuthNav /> : <NonAuthNav />)
+      }
+    </AuthUserContext.Consumer>
   </div>
 )
 
