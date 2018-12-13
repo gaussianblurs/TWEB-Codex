845a27dc64ceeee5828aaef0c0f7b0df7f199c64
GaussianBlurs <arnoldvbg@gmail.com>
2018-12-13 14:13:07 +0100

    Reafctoring

--- codex/frontend/src/components/Main.js
+++ codex/frontend/src/components/Main.js
@@ -1,30 +1,33 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import PropTypes from 'prop-types'
 import * as routes from '../constants/routes'
-import AuthUserContext from './AuthUserContext'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import PasswordForget from './pages/PasswordForget'
 import PasswordChange from './pages/PasswordChange'
 
-const Main = () => (
+const Main = props => (
   <div>
-    <AuthUserContext.Consumer>
-      {
-        // eslint-disable-next-line no-unused-vars
-        ({ authUser }) => (
-          <Switch>
-            <Route exact path={routes.HOME} component={Home} />
-            <Route exact path={routes.SIGN_UP} component={SignUp} />
-            <Route exact path={routes.SIGN_IN} component={SignIn} />
-            <Route exact path={routes.PASSWORD_FORGET} component={PasswordForget} />
-            <Route exact path={routes.PASSWORD_CHANGE} component={PasswordChange} />
-          </Switch>
-        )
-      }
-    </AuthUserContext.Consumer>
+    <Switch>
+      <Route exact path={routes.HOME} component={Home} />
+      <Route exact path={routes.SIGN_UP} component={SignUp} />
+      <Route exact path={routes.SIGN_IN} component={SignIn} />
+      <Route exact path={routes.PASSWORD_FORGET} component={PasswordForget} />
+      <Route exact path={routes.PASSWORD_CHANGE} component={PasswordChange} />
+    </Switch>
   </div>
 )
+
+Main.propTypes = {
+  authUser: PropTypes.shape({
+    uid: PropTypes.string.isRequired
+  })
+}
+
+Main.defaultProps = {
+  authUser: null
+}
 
 export default Main
