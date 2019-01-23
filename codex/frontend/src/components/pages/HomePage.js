099026c5566065cb2be91bff6f09fba1073f1c97
psr <pierre.samuel.rochat@gmail.com>
2019-01-23 14:45:37 +0100

    présentation

--- codex/frontend/src/components/pages/HomePage.js
+++ codex/frontend/src/components/pages/HomePage.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Layout } from 'antd'
 
+import logo from '../../assets/images/logo.svg'
 import '../../assets/scss/HomePage.scss'
 
 const { Content } = Layout
 
 const Home = () => (
   <Content className="main-container">
-    <h1>Home</h1>
+    <div className="home-logo">
+      <img className="img" height={150} src={logo} alt="home-logo" />
+      <div className="text">
+        <div className="title">CODEX</div>
+        <div className="subtitle">Share your best lines of code !</div>
+      </div>
+    </div>
   </Content>
 )
 
